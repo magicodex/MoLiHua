0c3094cc86a2b2cbac4e1525c3260e0686f9ad3b
magicodex <zhuangmaohui@163.com>
2021-11-16 22:40:00 +0800

    完善代码

--- src/main/java/jasmine/JasmineApplication.java
+++ src/main/java/jasmine/JasmineApplication.java
@@ -1,10 +1,12 @@
 package jasmine;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@MapperScan("jasmine.**.mapper")
 @EnableSwagger2
 @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages = {"jasmine"})
