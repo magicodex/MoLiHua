390cba1cbcf3de0bc78c1359c8d2c7e41257bc67
magicodex <zhuangmaohui@163.com>
2021-11-22 21:02:08 +0800

    完善代码

---
+++ src/test/java/jasmine/example/persistence/dao/ExampleDaoTest.java
@@ -0,0 +1,20 @@
+package jasmine.example.persistence.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import test.context.BeanTestContext;
+
+@RunWith(SpringRunner.class)
+public class ExampleDaoTest extends BeanTestContext {
+    @Autowired
+    private ExampleDao exampleDao;
+
+    @Test
+    public void testListAllExamples() {
+        Assert.assertEquals(0, exampleDao.listAllExamples().size());
+    }
+
+}
