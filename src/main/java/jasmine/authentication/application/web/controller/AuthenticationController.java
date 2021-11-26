0549b4aa240032ddacf11b1209cbe084f9792934
magicodex <zhuangmaohui@163.com>
2021-11-26 22:50:27 +0800

    完善代码

---
+++ src/main/java/jasmine/authentication/application/web/controller/AuthenticationController.java
@@ -0,0 +1,18 @@
+package jasmine.authentication.application.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author mh.z
+ */
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login.html");
+    }
+
+}
--- src/main/java/jasmine/framework/config/security/SpringSecurityConfig.java
+++ src/main/java/jasmine/framework/config/security/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import jasmine.framework.security.UserSubject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,6 +21,22 @@ import java.util.Collections;
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/static/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        http.csrf()
+                .disable();
+    }
 
     @Bean
     @Override
