38dabe28d37bf3edae4d9a841cef7757d66e339f
magicodex <zhuangmaohui@163.com>
2021-12-19 11:52:33 +0800

    完善代码

--- src/main/java/jasmine/JasmineApplication.java
+++ src/main/java/jasmine/JasmineApplication.java
@@ -1,6 +1,8 @@
 package jasmine;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,9 +23,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages = {"jasmine"})
 public class JasmineApplication {
+    private static final Logger logger = LoggerFactory.getLogger(JasmineApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(JasmineApplication.class);
+
+        logger.info("Server running...");
     }
 
 }
