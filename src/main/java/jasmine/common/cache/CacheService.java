7a4569bf581b0398b23497750261d27f19d5773a
magicodex <zhuangmaohui@163.com>
2021-12-20 21:28:15 +0800

    完善代码

---
+++ src/main/java/jasmine/common/cache/CacheService.java
@@ -0,0 +1,24 @@
+package jasmine.common.cache;
+
+/**
+ * @author mh.z
+ */
+public interface CacheService {
+
+    /**
+     * 从缓存中查找数据
+     *
+     * @param category
+     * @param key
+     */
+    void get(String category, String key);
+
+    /**
+     * 添加数据到缓存中
+     *
+     * @param category
+     * @param key
+     * @param value
+     */
+    void put(String category, String key, Object value);
+}
