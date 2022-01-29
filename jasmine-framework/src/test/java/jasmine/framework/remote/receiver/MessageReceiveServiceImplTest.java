b65ded6cdd36a430c12998e318e13dcb773e81bf
magicodex <zhuangmaohui@163.com>
2022-01-29 23:40:24 +0800

    完善代码

--- jasmine-framework/src/test/java/jasmine/framework/remote/receiver/MessageReceiveServiceImplTest.java
+++ jasmine-framework/src/test/java/jasmine/framework/remote/receiver/MessageReceiveServiceImplTest.java
@@ -14,6 +14,16 @@ public class MessageReceiveServiceImplTest {
 
         // 布尔值
         Assert.assertEquals(true, service.convertText("true", Boolean.class));
+        Assert.assertEquals(false, service.convertText("false", Boolean.class));
+        Assert.assertEquals(null, service.convertText("", Boolean.class));
+
+        // 字符
+        Assert.assertEquals('t', service.convertText("text", Character.class));
+        Assert.assertEquals(null, service.convertText("", Character.class));
+
+        // 长整型
+        Assert.assertEquals(Long.valueOf(1), service.convertText("1", Long.class));
+        Assert.assertEquals(null, service.convertText("", Long.class));
     }
 
 }
