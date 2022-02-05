3ec09542cbe9a7b045c5b0a8d634ff3646970aed
magicodex <zhuangmaohui@163.com>
2022-02-05 21:07:13 +0800

    完善代码

---
+++ jasmine-demo/src/test/java/jasmine/demo/auhentication/persistence/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package jasmine.demo.auhentication.persistence.dao;
+
+import jasmine.demo.authentication.persistent.dao.UserDao;
+import jasmine.demo.authentication.persistent.entity.UserEO;
+import jasmine.test.context.BeanTestContext;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author mh.z
+ */
+@RunWith(SpringRunner.class)
+public class UserDaoTest extends BeanTestContext {
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testGetUserByName() {
+        UserEO user = userDao.getUserByName("test");
+
+        Assert.assertNull(user);
+    }
+
+}
