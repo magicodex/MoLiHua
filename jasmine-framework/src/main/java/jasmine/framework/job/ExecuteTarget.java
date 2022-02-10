689c80305822823cbe5609c22f9565a03481d425
magicodex <zhuangmaohui@163.com>
2022-02-10 22:12:42 +0800

    添加调度任务接口类

---
+++ jasmine-framework/src/main/java/jasmine/framework/job/ExecuteTarget.java
@@ -0,0 +1,8 @@
+package jasmine.framework.job;
+
+/**
+ * @author mh.z
+ */
+public interface ExecuteTarget {
+
+}
---
+++ jasmine-framework/src/main/java/jasmine/framework/job/JobService.java
@@ -0,0 +1,14 @@
+package jasmine.framework.job;
+
+/**
+ * @author mh.z
+ */
+public interface JobService {
+
+    /**
+     * 执行
+     *
+     * @param target
+     */
+    void execute(ExecuteTarget target);
+}
