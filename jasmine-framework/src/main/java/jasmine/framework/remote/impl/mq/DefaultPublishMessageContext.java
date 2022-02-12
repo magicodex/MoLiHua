192f5f34d4dfe17529781fdb5be68179c0e9460a
magicodex <zhuangmaohui@163.com>
2022-02-12 16:21:31 +0800

    重新设计消息队列接口

---
+++ jasmine-framework/src/main/java/jasmine/framework/remote/impl/mq/DefaultPublishMessageContext.java
@@ -0,0 +1,61 @@
+package jasmine.framework.remote.impl.mq;
+
+import jasmine.framework.remote.mq.PublishMessageContext;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * @author mh.z
+ */
+public class DefaultPublishMessageContext implements PublishMessageContext {
+    private String key;
+    private Map<String, Object> attributes;
+
+    public DefaultPublishMessageContext() {
+        this(null);
+    }
+
+    public DefaultPublishMessageContext(String key) {
+        this.key = key;
+        this.attributes = new HashMap<>();
+    }
+
+    @Override
+    public String computeKey(Supplier<String> supplier) {
+        key = supplier.get();
+
+        return key;
+    }
+
+    @Override
+    public String computeKeyIfAbsent(Supplier<String> supplier) {
+        if (key == null) {
+            key = supplier.get();
+        }
+
+        return key;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return attributes.get(name);
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+}
---
+++ jasmine-framework/src/main/java/jasmine/framework/remote/impl/rabbit/RabbitPublishMessageService.java
@@ -0,0 +1,65 @@
+package jasmine.framework.remote.impl.rabbit;
+
+import jasmine.core.context.RuntimeProvider;
+import jasmine.core.util.QCheckUtil;
+import jasmine.framework.FrameworkConfig;
+import jasmine.framework.remote.impl.mq.DefaultPublishMessageContext;
+import jasmine.framework.remote.mq.PublishMessageContext;
+import jasmine.framework.remote.mq.PublishMessageInterceptor;
+import jasmine.framework.remote.mq.PublishMessageProvider;
+import jasmine.framework.remote.mq.PublishMessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author mh.z
+ */
+@Service
+public class RabbitPublishMessageService implements PublishMessageService {
+    private static final Logger logger = LoggerFactory.getLogger(RabbitPublishMessageService.class);
+    private RuntimeProvider runtimeProvider;
+    private FrameworkConfig config;
+    private PublishMessageInterceptor interceptor;
+
+    private static final String PROVIDER_SUFFIX = "PublishMessageProvider";
+
+    public RabbitPublishMessageService(RuntimeProvider runtimeProvider, FrameworkConfig config,
+                                       @Autowired(required = false) PublishMessageInterceptor interceptor) {
+        this.runtimeProvider = runtimeProvider;
+        this.config = config;
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void publish(String category, Object data) {
+        QCheckUtil.notNull(category, "category null");
+        QCheckUtil.notNull(category, "data null");
+
+
+        if (Boolean.TRUE.equals(config.getMessageQueuePublisherEnabled())) {
+            PublishMessageProvider provider = getProvider(category);
+
+            if (interceptor != null) {
+                interceptor.intercept(provider, category, data);
+            } else {
+                PublishMessageContext context = new DefaultPublishMessageContext();
+                provider.publish(context, data);
+            }
+        } else {
+            logger.warn("publish skipped(jasmine.message-queue.publisher.enabled=false)");
+        }
+    }
+
+    @Override
+    public PublishMessageProvider getProvider(String category) {
+        QCheckUtil.notNull(category, "category null");
+
+        String providerName = category + PROVIDER_SUFFIX;
+        PublishMessageProvider providerBean = runtimeProvider.getByName(providerName);
+
+        return providerBean;
+    }
+
+}
