412d37849cd5bef4a9ef7f929f8218d1e7a0995a
magicodex <zhuangmaohui@163.com>
2022-02-13 19:40:38 +0800

    完善权限方案

--- jasmine-demo/src/main/java/jasmine/demo/system/security/CustomUserDetailsService.java
+++ jasmine-demo/src/main/java/jasmine/demo/system/security/CustomUserDetailsService.java
@@ -1,16 +1,20 @@
 package jasmine.demo.system.security;
 
 import jasmine.core.context.RuntimeProvider;
+import jasmine.core.util.QCollectionUtil;
 import jasmine.demo.authentication.persistence.dao.UserDao;
 import jasmine.demo.authentication.persistence.entity.UserEO;
+import jasmine.security.rbac.model.SecurityRole;
+import jasmine.security.rbac.service.SecurityRoleService;
 import jasmine.security.subject.UserSubject;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collections;
+import java.util.List;
 
 /**
  * @author mh.z
@@ -19,11 +23,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     private RuntimeProvider runtimeProvider;
     private UserDao userDao;
     private PasswordEncoder passwordEncoder;
+    private SecurityRoleService roleService;
 
     public CustomUserDetailsService(RuntimeProvider provider) {
         this.runtimeProvider = provider;
         userDao = runtimeProvider.getByType(UserDao.class);
         passwordEncoder = runtimeProvider.getByType(PasswordEncoder.class);
+        roleService = runtimeProvider.getByType(SecurityRoleService.class);
     }
 
     @Override
@@ -33,9 +39,15 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("username '" + username + "' not found");
         }
 
-        UserSubject userDetails = new UserSubject(user.getTenantId(), user.getId(),
-                user.getUserName(), passwordEncoder.encode(user.getPassword()),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+        Long userId = user.getId();
+        List<SecurityRole> roleList = roleService.listRolesByUserId(userId);
+
+        List<GrantedAuthority> authorityList = QCollectionUtil.mapToList(roleList, (role) -> {
+            return new SimpleGrantedAuthority(role.getRoleCode());
+        });
+
+        UserSubject userDetails = new UserSubject(user.getTenantId(), userId,
+                user.getUserName(), passwordEncoder.encode(user.getPassword()), authorityList);
 
         return userDetails;
     }
