322cc92b8208af7652fbd538ee0f5402e5f6d390
magicodex <zhuangmaohui@163.com>
2022-02-19 00:23:57 +0800

    完善代码

--- jasmine-demo/src/main/resources/public/static/home/home.js
+++ jasmine-demo/src/main/resources/public/static/home/home.js
@@ -2,16 +2,8 @@
 (function () {
   'use strict'
 
-  // 初始主区域大小
-  $("main").css("height", $(document).height() - 48);
-
-  // 调整主区域大小
-  $(window).resize(function() {
-    $("main").css("height", $(document).height() - 48);
-  });
-
   // 点击菜单事件
-  $(".menu-item").click(function(event) {
+  $(".home-menu-item > a").click(function(event) {
     event.preventDefault();
 
     var link = $(this).attr("href");
