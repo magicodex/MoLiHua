5ce023a05200729f94e0f554d788516f919b9a0c
magicodex <zhuangmaohui@163.com>
2022-02-21 00:54:25 +0800

    完善代码

--- jasmine-demo/src/main/java/jasmine/demo/framework/config/JasmineFrameworkConfig.java
+++ jasmine-demo/src/main/java/jasmine/demo/framework/config/JasmineFrameworkConfig.java
@@ -1,11 +1,19 @@
 package jasmine.demo.framework.config;
 
 import jasmine.core.context.RuntimeProvider;
+import jasmine.demo.framework.security.CustomClientDetailsService;
+import jasmine.demo.framework.security.CustomRbacCheckService;
+import jasmine.demo.framework.security.CustomUserDetailsService;
 import jasmine.framework.JasmineFrameworkConfigTemplate;
 import jasmine.framework.remote.impl.mq.DefaultSendMessageService;
 import jasmine.framework.remote.impl.rabbit.RabbitReceiveMessageService;
 import jasmine.framework.remote.mq.ReceiveMessageService;
 import jasmine.framework.remote.mq.SendMessageService;
+import jasmine.security.JasmineSecurityConfigTemplate;
+import jasmine.security.authorization.RbacCheckService;
+import jasmine.security.rbac.service.SecurityResourceService;
+import jasmine.security.subject.ClientDetailsServiceProvider;
+import jasmine.security.subject.UserDetailsServiceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,18 +22,43 @@ import org.springframework.context.annotation.Configuration;
  * @author mh.z
  */
 @Configuration
-public class JasmineFrameworkConfig implements JasmineFrameworkConfigTemplate {
+public class JasmineFrameworkConfig implements JasmineFrameworkConfigTemplate, JasmineSecurityConfigTemplate {
     @Autowired
     private RuntimeProvider runtimeProvider;
 
-    @Bean
     @Autowired
+    private SecurityResourceService resourceService;
+
+    @Bean
+    @Override
+    public ClientDetailsServiceProvider clientDetailsServiceProvider() {
+        return () -> {
+            return new CustomClientDetailsService(runtimeProvider);
+        };
+    }
+
+    @Bean
+    @Override
+    public UserDetailsServiceProvider userDetailsServiceProvider() {
+        return () -> {
+            return new CustomUserDetailsService(runtimeProvider);
+        };
+    }
+
+    @Bean
+    @Override
+    public RbacCheckService rbacCheckService() {
+        return new CustomRbacCheckService(resourceService);
+    }
+
+    @Bean
+    @Override
     public ReceiveMessageService receiveMessageService() {
         return new RabbitReceiveMessageService(runtimeProvider);
     }
 
     @Bean
-    @Autowired
+    @Override
     public SendMessageService sendMessageService() {
         return new DefaultSendMessageService(runtimeProvider);
     }
