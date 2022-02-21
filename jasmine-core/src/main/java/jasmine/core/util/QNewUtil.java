54a6dcd2ef030bf9e179cfc0febde0f15db5165e
magicodex <zhuangmaohui@163.com>
2022-02-22 05:06:59 +0800

    完善代码

--- jasmine-core/src/main/java/jasmine/core/util/QNewUtil.java
+++ jasmine-core/src/main/java/jasmine/core/util/QNewUtil.java
@@ -1,6 +1,8 @@
 package jasmine.core.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,6 +14,11 @@ import java.util.Map;
  */
 public class QNewUtil {
 
+    //
+    // TreeMap 的 key 不能为 null。
+    // ConcurrentHashMap 的 key 和 value 都不能为 null。
+    //
+
     /**
      * 创建 Map 对象并返回
      *
@@ -21,6 +28,39 @@ public class QNewUtil {
      */
     public static <K, V> Map<K, V> map() {
         return new HashMap<>(16);
+    }
+
+    /**
+     * 创建 Map 对象并返回
+     *
+     * @param initialCapacity
+     * @param <K>
+     * @param <V>
+     * @return
+     */
+    public static <K, V> Map<K, V> map(int initialCapacity) {
+        return new HashMap<>(initialCapacity);
+    }
+
+    /**
+     * 创建 List 对象并返回
+     *
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> list() {
+        return new ArrayList<>(10);
+    }
+
+    /**
+     * 创建 List 对象并返回
+     *
+     * @param initialCapacity
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> list(int initialCapacity) {
+        return new ArrayList<>(10);
     }
 
 }
--- jasmine-security/src/main/java/jasmine/security/config/OAuth2AuthorizationConfig.java
+++ jasmine-security/src/main/java/jasmine/security/config/OAuth2AuthorizationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
 
 /**
  * <p>
@@ -33,8 +34,11 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // 开启 /oauth/token_key 验证端口成无权限访问
         security.tokenKeyAccess("permitAll()")
+                // 开启 /oauth/check_token 验证端口成认证权限访问
                 .checkTokenAccess("isAuthenticated()")
+                // 主要是让 /oauth/token 支持 client_id 以及 client_secret 做登录认证
                 .allowFormAuthenticationForClients();
     }
 
@@ -42,6 +46,8 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         if (clientDetailsServiceProvider != null) {
             clients.withClientDetails(clientDetailsServiceProvider.getService());
+        } else {
+            clients.withClientDetails(new InMemoryClientDetailsService());
         }
     }
 
