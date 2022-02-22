6e4bd79d65ba2c786f33977242a7d843bfa5b49f
magicodex <zhuangmaohui@163.com>
2022-02-22 08:21:38 +0800

    完善安全认证代码

--- jasmine-demo/src/main/java/jasmine/demo/framework/security/BaseClientDetailsService.java
+++ jasmine-demo/src/main/java/jasmine/demo/framework/security/BaseClientDetailsService.java
@@ -1,8 +1,13 @@
 package jasmine.demo.framework.security;
 
 import jasmine.core.context.RuntimeProvider;
+import jasmine.core.util.QCollectionUtil;
 import jasmine.demo.authentication.persistence.dao.UserDao;
 import jasmine.demo.authentication.persistence.entity.UserEO;
+import jasmine.security.authorization.RoleAuthority;
+import jasmine.security.rbac.model.SecurityRole;
+import jasmine.security.rbac.service.SecurityRoleService;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -10,6 +15,7 @@ import org.springframework.security.oauth2.provider.ClientRegistrationException;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author mh.z
@@ -17,11 +23,13 @@ import java.util.Arrays;
 public class BaseClientDetailsService implements ClientDetailsService {
     private RuntimeProvider runtimeProvider;
     private UserDao userDao;
+    private SecurityRoleService roleService;
     private PasswordEncoder passwordEncoder;
 
     public BaseClientDetailsService(RuntimeProvider provider) {
         this.runtimeProvider = provider;
         userDao = runtimeProvider.getByType(UserDao.class);
+        roleService = runtimeProvider.getByType(SecurityRoleService.class);
         passwordEncoder = runtimeProvider.getByType(PasswordEncoder.class);
     }
 
@@ -32,12 +40,21 @@ public class BaseClientDetailsService implements ClientDetailsService {
             return null;
         }
 
+        // 获取角色
+        Long userId = user.getId();
+        List<SecurityRole> roleList = roleService.listRolesByUserId(userId);
+
+        List<GrantedAuthority> authorityList = QCollectionUtil.mapToList(roleList, (role) -> {
+            return new RoleAuthority(role.getId(), role.getRoleCode());
+        });
+
         BaseClientDetails clientDetails = new BaseClientDetails();
         clientDetails.setClientId(clientId);
         clientDetails.setClientSecret(passwordEncoder.encode(user.getPassword()));
+        clientDetails.setAuthorities(authorityList);
         clientDetails.setAuthorizedGrantTypes(Arrays.asList("authorization_code", "password", "refresh_token"));
-        // "ALL"是自定义，没有特殊含义
-        clientDetails.setScope(Arrays.asList("ALL"));
+        // "CLIENT"是自定义，没有特殊含义
+        clientDetails.setScope(Arrays.asList("CLIENT"));
         clientDetails.setAccessTokenValiditySeconds(3600);
 
         return clientDetails;
