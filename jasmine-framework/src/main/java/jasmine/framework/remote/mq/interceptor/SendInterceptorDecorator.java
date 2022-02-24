c98b88241c8a8e6a711b93dcc9e663b4f7571afc
magicodex <zhuangmaohui@163.com>
2022-02-24 20:47:56 +0800

    完善代码

---
+++ jasmine-framework/src/main/java/jasmine/framework/remote/mq/interceptor/SendInterceptorDecorator.java
@@ -0,0 +1,14 @@
+package jasmine.framework.remote.mq.interceptor;
+
+/**
+ * @author mh.z
+ */
+public interface SendInterceptorDecorator {
+
+    /**
+     * 装饰拦截器
+     *
+     * @return
+     */
+    SendInterceptor decorate(SendInterceptor interceptor);
+}
