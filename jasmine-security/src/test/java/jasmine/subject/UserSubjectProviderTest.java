5863e87f51d42e4323382341d2c8ef7050945abd
magicodex <zhuangmaohui@163.com>
2022-02-26 00:10:39 +0800

    增加测试用例

---
+++ jasmine-security/src/test/java/jasmine/subject/UserSubjectProviderTest.java
@@ -0,0 +1,34 @@
+package jasmine.subject;
+
+import jasmine.security.subject.UserSubject;
+import jasmine.security.subject.UserSubjectDetailsService;
+import jasmine.security.subject.UserSubjectProvider;
+import jasmine.test.mockito.MockUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author mh.z
+ */
+public class UserSubjectProviderTest {
+
+    @Test
+    public void test() {
+        UserSubjectDetailsService service = MockUtil.mock(UserSubjectDetailsService.class, (target) -> {
+            Mockito.when(target.loadUserByUserId(Mockito.anyLong()))
+                    .thenAnswer((invocationOnMock) -> {
+                        Long userId = invocationOnMock.getArgument(0);
+
+                        return new UserSubject(100001L, userId);
+                    });
+        });
+
+        UserSubjectProvider provider = new UserSubjectProvider(service);
+        provider.setCurrentSubject(null, 100100L);
+
+        Assert.assertEquals(Long.valueOf(100001L), provider.getCurrentTenantId());
+        Assert.assertEquals(Long.valueOf(100100L), provider.getCurrentUserId());
+    }
+
+}
