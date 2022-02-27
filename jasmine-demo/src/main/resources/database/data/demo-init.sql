8f7ad463b0c3d42da2a9ce02c45a4475efefa55f
magicodex <zhuangmaohui@163.com>
2022-02-27 23:40:36 +0800

    重写demo模块

--- jasmine-demo/src/main/resources/database/data/demo-init.sql
+++ jasmine-demo/src/main/resources/database/data/demo-init.sql
@@ -1,6 +1,3 @@
 INSERT INTO `sys_user` VALUES (100001, 'MoLiHua', '123456', 1, '2022-02-16 12:30:00', -1, '2022-02-16 12:30:00', -1, 1);
 INSERT INTO `sec_resource` VALUES (1, 'API', 'ANONYMOUS', '*', '/login', 0, NULL, '2022-02-16 12:30:00', -1, '2022-02-16 12:30:00', -1, 1);
 INSERT INTO `sec_resource` VALUES (2, 'PAGE', 'AUTHENTICATED', 'GET', '/home', 0, NULL, '2022-02-16 12:30:00', -1, '2022-02-16 12:30:00', -1, 1);
-INSERT INTO `sec_resource` VALUES (3, 'PAGE', 'AUTHENTICATED', '*', '/journal/search', 0, NULL, '2022-02-19 12:30:00', -1, '2022-02-19 12:30:00', -1, 1);
-INSERT INTO `sec_resource` VALUES (4, 'PAGE', 'AUTHENTICATED', '*', '/journal/edit', 0, NULL, '2022-02-19 12:30:00', -1, '2022-02-19 12:30:00', -1, 1);
-INSERT INTO `sec_resource` VALUES (5, 'PAGE', 'AUTHENTICATED', '*', '/journal/save', 0, NULL, '2022-02-19 12:30:00', -1, '2022-02-19 12:30:00', -1, 1);
