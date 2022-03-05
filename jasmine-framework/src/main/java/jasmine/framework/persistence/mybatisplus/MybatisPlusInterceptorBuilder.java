3058a5b1f7eccc7a912f12c5deb9962492e307bd
magicodex <zhuangmaohui@163.com>
2022-03-05 17:43:33 +0800

    添加租户拦截器

--- jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/MybatisPlusInterceptorBuilder.java
+++ jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/MybatisPlusInterceptorBuilder.java
@@ -1,9 +1,11 @@
 package jasmine.framework.persistence.mybatisplus;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import jasmine.core.context.SubjectProvider;
 import jasmine.core.util.QCheckUtil;
 import jasmine.framework.persistence.mybatisplus.context.ContextParameter;
@@ -15,9 +17,19 @@ import jasmine.framework.persistence.mybatisplus.context.DefaultContextParameter
  */
 public class MybatisPlusInterceptorBuilder {
     private SubjectProvider subjectProvider;
+    private Boolean tenantEnabled;
+    private TenantLineHandler tenantLineHandler;
 
     public MybatisPlusInterceptorBuilder(SubjectProvider subjectProvider) {
         this.subjectProvider = subjectProvider;
+    }
+
+    public void setTenantEnabled(Boolean tenantEnabled) {
+        this.tenantEnabled = tenantEnabled;
+    }
+
+    public void setTenantLineHandler(TenantLineHandler tenantLineHandler) {
+        this.tenantLineHandler = tenantLineHandler;
     }
 
     /**
@@ -35,6 +47,12 @@ public class MybatisPlusInterceptorBuilder {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         // 防止全表更新与删除插件
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+
+        // 租户拦截器
+        if (Boolean.TRUE.equals(tenantEnabled)) {
+            QCheckUtil.notNullProp(tenantLineHandler, "tenantLineHandler null");
+            interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(tenantLineHandler));
+        }
 
         ContextParameter contextParameter = new DefaultContextParameter(subjectProvider);
         interceptor.addInnerInterceptor(new ContextParameterInnerInterceptor(contextParameter));
