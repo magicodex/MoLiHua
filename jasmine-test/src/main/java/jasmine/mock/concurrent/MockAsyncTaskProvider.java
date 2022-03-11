222f320ac506899780b5e1e822515ca38bd23101
magicodex <zhuangmaohui@163.com>
2022-03-11 14:04:39 +0800

    完善代码

--- jasmine-test/src/main/java/jasmine/mock/concurrent/MockAsyncTaskProvider.java
+++ jasmine-test/src/main/java/jasmine/mock/concurrent/MockAsyncTaskProvider.java
@@ -1,5 +1,34 @@
 package jasmine.mock.concurrent;
 
-public class MockAsyncTaskProvider {
+import jasmine.core.util.QCollectionUtil;
+import jasmine.framework.concurrent.AsyncTaskProvider;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * @author mh.z
+ */
+public class MockAsyncTaskProvider implements AsyncTaskProvider {
+
+
+    @Override
+    public void async(Runnable task) {
+        task.run();
+    }
+
+    @Override
+    public <T> List<T> asyncAndGet(Collection<Callable> tasks) {
+        List<Object> resultList = QCollectionUtil.forEach(tasks, (task) -> {
+            try {
+                return task.call();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return (List<T>) resultList;
+    }
 
 }
--- jasmine-test/src/main/java/jasmine/mock/testconfigure/support/AsyncTaskTestConfiguration.java
+++ jasmine-test/src/main/java/jasmine/mock/testconfigure/support/AsyncTaskTestConfiguration.java
@@ -1,8 +1,8 @@
 package jasmine.mock.testconfigure.support;
 
-import jasmine.framework.concurrent.AsyncExecutorTaskProvider;
 import jasmine.framework.concurrent.AsyncTaskProvider;
 import jasmine.framework.concurrent.AsyncTaskUtil;
+import jasmine.mock.concurrent.MockAsyncTaskProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,7 +22,7 @@ public class AsyncTaskTestConfiguration {
 
     @Bean
     public AsyncTaskProvider asyncTaskProvider(Executor executor) {
-        AsyncTaskProvider provider = new AsyncExecutorTaskProvider(executor);
+        AsyncTaskProvider provider = new MockAsyncTaskProvider();
         // 初始工具类
         AsyncTaskUtil.initUtil(provider);
 
