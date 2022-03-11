236fee9c7cdf2c0bf91a40fc90d4c16afacc55fc
magicodex <zhuangmaohui@163.com>
2022-03-12 02:30:02 +0800

    完善DEMO

---
+++ jasmine-demo/src/main/java/jasmine/demo/sample/SampleJobHandler.java
@@ -0,0 +1,26 @@
+package jasmine.demo.sample;
+
+import jasmine.framework.job.JobCurrent;
+import jasmine.framework.job.JobExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mh.z
+ */
+@Component
+public class SampleJobHandler implements JobExecutor {
+    private static Logger logger = LoggerFactory.getLogger(SampleJobHandler.class);
+
+    @Override
+    public String getName() {
+        return "sampleJobHandler";
+    }
+
+    @Override
+    public void execute(JobCurrent job) {
+        logger.info("job(sampleJobHandler) execute.");
+    }
+
+}
