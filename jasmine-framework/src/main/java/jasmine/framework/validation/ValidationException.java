c1cd30c1c6ddb562d7db83d374ef2fc4402efec7
magicodex <zhuangmaohui@163.com>
2022-03-12 20:36:26 +0800

    完善代码

--- jasmine-framework/src/main/java/jasmine/framework/validation/ValidationException.java
+++ jasmine-framework/src/main/java/jasmine/framework/validation/ValidationException.java
@@ -1,32 +1,34 @@
 package jasmine.framework.validation;
 
 import jasmine.core.exception.ApplicationException;
-import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+
+import java.util.Collection;
 
 /**
  * @author mh.z
  */
 public class ValidationException extends ApplicationException {
-    private Errors errors;
+    private Collection<ObjectError> errors;
 
     /** 默认错误代码 */
     private static final String DEFAULT_ERROR_CODE = "VALIDATE_FAILED";
 
-    public ValidationException(Errors errors) {
+    public ValidationException(Collection<ObjectError> errors) {
         super();
         this.errors = errors;
         this.errorCode = DEFAULT_ERROR_CODE;
         this.errorDetail = buildErrorDetail(errors);
     }
 
-    public ValidationException(String messageOrKey, Errors errors) {
+    public ValidationException(String messageOrKey, Collection<ObjectError> errors) {
         super(messageOrKey);
         this.errors = errors;
         this.errorCode = DEFAULT_ERROR_CODE;
         this.errorDetail = buildErrorDetail(errors);
     }
 
-    public Errors getErrors() {
+    public Collection<ObjectError> getErrors() {
         return errors;
     }
 
@@ -36,7 +38,7 @@ public class ValidationException extends ApplicationException {
      * @param errors
      * @return
      */
-    public static String buildErrorDetail(Errors errors) {
+    public static String buildErrorDetail(Collection<ObjectError> errors) {
         return errors.toString();
     }
 
