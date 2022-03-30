3f75f2faa8ff9d113cb4a3eede9a94bee3e6a161
magicodex <zhuangmaohui@163.com>
2022-03-31 00:59:38 +0800

    完善代码

--- jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/dao/BaseDAO.java
+++ jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/dao/BaseDAO.java
@@ -1,8 +1,41 @@
 package jasmine.framework.persistence.mybatisplus.dao;
 
+import java.io.Serializable;
+
 /**
  * @author mh.z
  */
 public interface BaseDAO<T> {
 
+    /**
+     * 保存记录
+     *
+     * @param entity
+     * @return
+     */
+    boolean save(T entity);
+
+    /**
+     * 查询记录
+     *
+     * @param id
+     * @return
+     */
+    T getById(Serializable id);
+
+    /**
+     * 更新记录
+     *
+     * @param entity
+     * @return
+     */
+    boolean updateById(T entity);
+
+    /**
+     * 删除记录
+     *
+     * @param id
+     * @return
+     */
+    boolean deleteById(Serializable id);
 }
--- jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/dao/BaseDaoSupport.java
+++ jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/dao/BaseDaoSupport.java
@@ -1,7 +1,10 @@
 package jasmine.framework.persistence.mybatisplus.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
 
 /**
  * @author mh.z
@@ -9,4 +12,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class BaseDaoSupport<M extends BaseMapper<T>, T> implements BaseDAO<T> {
     @Autowired
     protected M baseMapper;
+
+    @Override
+    public boolean save(T entity) {
+        return SqlHelper.retBool(baseMapper.insert(entity));
+    }
+
+    @Override
+    public T getById(Serializable id) {
+        return baseMapper.selectById(id);
+    }
+
+    @Override
+    public boolean updateById(T entity) {
+        return SqlHelper.retBool(baseMapper.updateById(entity));
+    }
+
+    @Override
+    public boolean deleteById(Serializable id) {
+        return SqlHelper.retBool(baseMapper.deleteById(id));
+    }
+
 }
