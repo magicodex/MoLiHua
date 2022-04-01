81aef630d70a41804443fdfc658705bb1fe42620
magicodex <zhuangmaohui@163.com>
2022-04-01 13:45:55 +0800

    完善代码

---
+++ jasmine-core/src/test/java/jasmine/core/util/sort/CollectionSortUtilTest.java
@@ -0,0 +1,30 @@
+package jasmine.core.util.sort;
+
+import jasmine.core.testdependency.Example1;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mh.z
+ */
+public class CollectionSortUtilTest {
+
+    @Test
+    public void test() {
+        List<Example1> exampleList = new ArrayList<>();
+        exampleList.add(Example1.create("example1", 1002, null));
+        exampleList.add(Example1.create("example2", 1001, null));
+        exampleList.add(Example1.create("example3", 1002, null));
+
+        List<Example1> actualList = CollectionSortUtil.sort(exampleList,
+                SortKey.asc(Example1::getInteger1), SortKey.desc(Example1::getString1));
+
+        Assert.assertEquals("example2", actualList.get(0).getString1());
+        Assert.assertEquals("example3", actualList.get(1).getString1());
+        Assert.assertEquals("example1", actualList.get(2).getString1());
+    }
+
+}
