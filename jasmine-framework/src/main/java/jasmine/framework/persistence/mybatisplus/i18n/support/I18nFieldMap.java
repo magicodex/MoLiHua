3c0459214a9f8b68bce27ca1f813e273f0607bb0
magicodex <zhuangmaohui@163.com>
2022-04-13 22:11:19 +0800

    完善代码

---
+++ jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/i18n/support/I18nFieldMap.java
@@ -0,0 +1,11 @@
+package jasmine.framework.persistence.mybatisplus.i18n.support;
+
+import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
+
+/**
+ * @author mh.z
+ */
+public class I18nFieldMap extends LinkedHashMap<String, Field> {
+
+}
