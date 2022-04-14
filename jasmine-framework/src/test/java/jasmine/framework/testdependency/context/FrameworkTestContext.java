0429e87bad3b25d6843cc70c8cbdc78bfdc3b1e5
magicodex <zhuangmaohui@163.com>
2022-04-14 21:25:01 +0800

    完善代码

---
+++ jasmine-framework/src/test/java/jasmine/framework/testdependency/context/FrameworkTestContext.java
@@ -0,0 +1,24 @@
+package jasmine.framework.testdependency.context;
+
+import jasmine.core.context.CurrentSubject;
+import org.junit.Before;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <p>
+ * 支持框架测试。
+ * </p>
+ *
+ * @author mh.z
+ */
+@Transactional(rollbackFor = Exception.class)
+@ContextConfiguration(locations = "classpath:/test/config/springContext.xml")
+public class FrameworkTestContext {
+
+    @Before
+    public void initOrResetBeforeTest() {
+        CurrentSubject.setSubject(1L, null);
+    }
+
+}
