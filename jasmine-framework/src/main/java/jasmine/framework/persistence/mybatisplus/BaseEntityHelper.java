e2f2f12582b7d3be24f79b7e6ae5905c009d5edc
magicodex <zhuangmaohui@163.com>
2022-04-23 19:58:47 +0800

    完善代码

---
+++ jasmine-framework/src/main/java/jasmine/framework/persistence/mybatisplus/BaseEntityHelper.java
@@ -0,0 +1,140 @@
+package jasmine.framework.persistence.mybatisplus;
+
+import jasmine.core.context.CurrentSubject;
+import jasmine.core.util.QCheckUtil;
+import jasmine.core.util.QCollUtil;
+import jasmine.core.util.QI18nUtil;
+import jasmine.framework.persistence.entity.BaseEntity;
+import jasmine.framework.persistence.entity.BaseI18nEntity;
+
+import java.time.ZonedDateTime;
+import java.util.Collection;
+
+/**
+ * @author mh.z
+ */
+public class BaseEntityHelper {
+
+    /**
+     * 填充字段
+     *
+     * @param entity
+     */
+    public static void fillInsert(BaseEntity entity) {
+        QCheckUtil.notNull(entity, "entity null");
+        Long userId = CurrentSubject.getUserId();
+        ZonedDateTime nowTime = ZonedDateTime.now();
+
+        // 创建人ID
+        entity.setCreatedBy(userId);
+        // 创建日期
+        entity.setCreatedDate(nowTime);
+        // 最后更新人ID
+        entity.setLastUpdatedBy(userId);
+        // 最后更新日期
+        entity.setLastUpdatedDate(nowTime);
+    }
+
+    /**
+     * 填充字段
+     *
+     * @param entities
+     */
+    public static void fillInsert(Collection<? extends BaseEntity> entities) {
+        QCheckUtil.notNull(entities, "entities null");
+
+        if (QCollUtil.isEmpty(entities)) {
+            return;
+        }
+
+        Long userId = CurrentSubject.getUserId();
+        ZonedDateTime nowTime = ZonedDateTime.now();
+
+        if (QCollUtil.getFirst(entities) instanceof BaseI18nEntity) {
+            String langCode = QI18nUtil.getLanguage();
+
+            entities.forEach((entity) -> {
+                // 创建人ID
+                entity.setCreatedBy(userId);
+                // 创建日期
+                entity.setCreatedDate(nowTime);
+                // 最后更新人ID
+                entity.setLastUpdatedBy(userId);
+                // 最后更新日期
+                entity.setLastUpdatedDate(nowTime);
+                // 语言代码
+                BaseI18nEntity baseI18nEntity = (BaseI18nEntity) entity;
+                baseI18nEntity.setLangCode(langCode);
+            });
+        } else {
+            entities.forEach((entity) -> {
+                // 创建人ID
+                entity.setCreatedBy(userId);
+                // 创建日期
+                entity.setCreatedDate(nowTime);
+                // 最后更新人ID
+                entity.setLastUpdatedBy(userId);
+                // 最后更新日期
+                entity.setLastUpdatedDate(nowTime);
+            });
+        }
+    }
+
+    /**
+     * 填充字段
+     *
+     * @param entity
+     */
+    public static void fillUpdate(BaseEntity entity) {
+        QCheckUtil.notNull(entity, "entity null");
+
+        // 最后更新人ID
+        entity.setLastUpdatedBy(CurrentSubject.getUserId());
+        // 最后更新日期
+        entity.setLastUpdatedDate(ZonedDateTime.now());
+
+        if (entity instanceof BaseI18nEntity) {
+            BaseI18nEntity baseI18nEntity = (BaseI18nEntity) entity;
+            // 语言代码
+            baseI18nEntity.setLangCode(QI18nUtil.getLanguage());
+        }
+    }
+
+    /**
+     * 填充字段
+     *
+     * @param entities
+     */
+    public static void fillUpdate(Collection<? extends BaseEntity> entities) {
+        QCheckUtil.notNull(entities, "entities null");
+
+        if (QCollUtil.isEmpty(entities)) {
+            return;
+        }
+
+        Long userId = CurrentSubject.getUserId();
+        ZonedDateTime nowTime = ZonedDateTime.now();
+
+        if (QCollUtil.getFirst(entities) instanceof BaseI18nEntity) {
+            String langCode = QI18nUtil.getLanguage();
+
+            entities.forEach((entity) -> {
+                // 最后更新人ID
+                entity.setLastUpdatedBy(userId);
+                // 最后更新日期
+                entity.setLastUpdatedDate(nowTime);
+                // 语言代码
+                BaseI18nEntity baseI18nEntity = (BaseI18nEntity) entity;
+                baseI18nEntity.setLangCode(langCode);
+            });
+        } else {
+            entities.forEach((entity) -> {
+                // 最后更新人ID
+                entity.setLastUpdatedBy(userId);
+                // 最后更新日期
+                entity.setLastUpdatedDate(nowTime);
+            });
+        }
+    }
+
+}
