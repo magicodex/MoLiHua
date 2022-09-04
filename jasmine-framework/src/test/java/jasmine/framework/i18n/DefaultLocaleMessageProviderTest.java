e67b3d3bba985d2d9164acd5ae1ea10cbce67133
magicodex <zhuangmaohui@163.com>
2022-09-05 00:39:44 +0800

    补充单元测试用例

---
+++ jasmine-framework/src/test/java/jasmine/framework/i18n/DefaultLocaleMessageProviderTest.java
@@ -0,0 +1,66 @@
+package jasmine.framework.i18n;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Locale;
+
+/**
+ * @author mh.z
+ */
+public class DefaultLocaleMessageProviderTest {
+    private Locale previousLocale;
+
+    @Before
+    public void setUp() {
+        previousLocale = LocaleContextHolder.getLocale();
+        LocaleContextHolder.setLocale(Locale.SIMPLIFIED_CHINESE);
+    }
+
+    @After
+    public void tearDown() {
+        LocaleContextHolder.setLocale(previousLocale);
+    }
+
+    @Test
+    public void testGetMessage() {
+        MessageSource messageSource = Mockito.mock(MessageSource.class);
+        // 这里只是把 messageKey 转换成大写
+        Mockito.when(messageSource.getMessage(Mockito.any(), Mockito.any(), Mockito.any()))
+                .then((answer) -> {
+                    String messageKey = answer.getArgument(0);
+
+                    return messageKey.toUpperCase();
+                });
+
+        LocaleContextHolder.setLocale(Locale.SIMPLIFIED_CHINESE);
+        DefaultLocaleMessageProvider provider = new DefaultLocaleMessageProvider(messageSource);
+
+        {
+            String actual = provider.getMessage("Hello, world!");
+            Assert.assertEquals("HELLO, WORLD!", actual);
+        }
+
+        {
+            String actual = provider.getMessage("$Hello, world!");
+            Assert.assertEquals("HELLO, WORLD!", actual);
+        }
+    }
+
+    @Test
+    public void testGetLanguage() {
+        MessageSource messageSource = Mockito.mock(MessageSource.class);
+        DefaultLocaleMessageProvider provider = new DefaultLocaleMessageProvider(messageSource);
+
+        LocaleContextHolder.setLocale(Locale.SIMPLIFIED_CHINESE);
+        String actual = provider.getLanguage();
+
+        Assert.assertEquals(Locale.SIMPLIFIED_CHINESE.toLanguageTag(), actual);
+    }
+
+}
---
+++ jasmine-framework/src/test/java/jasmine/framework/i18n/MessageSourceBuilderTest.java
@@ -0,0 +1,31 @@
+package jasmine.framework.i18n;
+
+import jasmine.framework.i18n.testdependency.TestMessages;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+/**
+ * @author mh.z
+ */
+public class MessageSourceBuilderTest {
+    private static final String RESOURCE_PREFIX = "test/framework/i18n/messages";
+    private static final String SCAN_PATH = "classpath:/jasmine/framework" +
+            "/i18n/testdependency/TestMessages.class";
+
+    @Test
+    public void test() {
+        MessageSourceBuilder builder = new MessageSourceBuilder();
+        builder.setResourcePrefix(RESOURCE_PREFIX);
+        builder.setEncoding("UTF-8");
+        builder.setConstantPattern(SCAN_PATH);
+
+        MessageSource messageSource = builder.build();
+        String actual = messageSource.getMessage(TestMessages.MESSAGE_1,
+                new Object[0], Locale.SIMPLIFIED_CHINESE);
+        Assert.assertEquals("消息1", actual);
+    }
+
+}
