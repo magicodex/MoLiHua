5bb994c539791e5f0d1a73dc205eb22fd85a7dee
magicodex <zhuangmaohui@163.com>
2022-09-06 23:37:14 +0800

    补充单元测试用例

--- jasmine-framework/src/test/java/jasmine/framework/job/xxljob/XxlJobCurrentTest.java
+++ jasmine-framework/src/test/java/jasmine/framework/job/xxljob/XxlJobCurrentTest.java
@@ -1,6 +1,9 @@
 package jasmine.framework.job.xxljob;
 
+import com.xxl.job.core.context.XxlJobContext;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
@@ -9,6 +12,29 @@ import java.util.Map;
  * @author mh.z
  */
 public class XxlJobCurrentTest {
+    private XxlJobContext previousContext;
+
+    @Before
+    public void setUp() {
+        previousContext = XxlJobContext.getXxlJobContext();
+        XxlJobContext.setXxlJobContext(null);
+    }
+
+    @After
+    public void tearDown() {
+        XxlJobContext.setXxlJobContext(previousContext);
+    }
+
+    @Test
+    public void testGetParameter() {
+        XxlJobContext jobContext = new XxlJobContext(1L, "param1:value1,param2:value2",
+                "", -1, -1);
+        XxlJobContext.setXxlJobContext(jobContext);
+
+        XxlJobCurrent current = new XxlJobCurrent();
+        Assert.assertEquals("value1", current.getParameter("param1"));
+        Assert.assertEquals("value2", current.getParameter("param2"));
+    }
 
     @Test
     public void testParseParameters() {
---
+++ jasmine-framework/src/test/java/jasmine/framework/job/xxljob/XxlJobHandlerDelegateTest.java
@@ -0,0 +1,92 @@
+package jasmine.framework.job.xxljob;
+
+import com.xxl.job.core.context.XxlJobContext;
+import jasmine.core.context.CurrentSubject;
+import jasmine.core.context.SubjectProvider;
+import jasmine.core.util.wrapper.ObjectValue;
+import jasmine.framework.context.ContextManagementHelper;
+import jasmine.framework.context.handler.ContextHandlerFacade;
+import jasmine.framework.job.JobCurrent;
+import jasmine.framework.job.JobExecutor;
+import jasmine.mock.core.context.MockSubjectProvider;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author mh.z
+ */
+public class XxlJobHandlerDelegateTest {
+    private XxlJobContext prevXxlJobContext;
+    private SubjectProvider prevSubjectProvider;
+    private ContextHandlerFacade prevContextHandlerFacade;
+
+    private ObjectValue jobExecutorExecuteMethodCalledFlag;
+
+    @Before
+    public void setUp() {
+        prevXxlJobContext = XxlJobContext.getXxlJobContext();
+        XxlJobContext.setXxlJobContext(null);
+
+        prevSubjectProvider = CurrentSubject.getSubjectProvider();
+        CurrentSubject.initUtil(null);
+
+        prevContextHandlerFacade = ContextManagementHelper.getHandlerFacade();
+        ContextManagementHelper.initUtil(null);
+
+        jobExecutorExecuteMethodCalledFlag = new ObjectValue(false);
+    }
+
+    @After
+    public void tearDown() {
+        XxlJobContext.setXxlJobContext(prevXxlJobContext);
+        CurrentSubject.initUtil(prevSubjectProvider);
+        ContextManagementHelper.initUtil(prevContextHandlerFacade);
+
+        jobExecutorExecuteMethodCalledFlag = new ObjectValue(false);
+    }
+
+    @Test
+    public void test() throws Exception {
+        // 初始 XxlJobContext 的上下文
+        XxlJobContext jobContext = new XxlJobContext(1L, "param1:value1,tenantId:666666",
+                "", -1, -1);
+        XxlJobContext.setXxlJobContext(jobContext);
+        // 初始 CurrentSubject 的上下文
+        CurrentSubject.initUtil(new MockSubjectProvider());
+        // 初始 ContextManagementHelper 的上下文
+        ContextManagementHelper.initUtil(Mockito.mock(ContextHandlerFacade.class));
+
+        JobExecutor executor = mockJobExecutor();
+        XxlJobHandlerDelegate delegate = new XxlJobHandlerDelegate(executor);
+        delegate.execute();
+        Assert.assertEquals(true, jobExecutorExecuteMethodCalledFlag.get());
+
+        // 检查当前的租户ID
+        Assert.assertEquals(Long.valueOf(666666), CurrentSubject.getTenantId());
+    }
+
+    /**
+     * 模拟 JobExecutor 对象
+     *
+     * @return
+     */
+    private JobExecutor mockJobExecutor() {
+        JobExecutor executor = new JobExecutor() {
+            @Override
+            public String getName() {
+                return "testJobExecutor";
+            }
+
+            @Override
+            public void execute(JobCurrent job) {
+                jobExecutorExecuteMethodCalledFlag.set(true);
+            }
+        };
+
+        return executor;
+    }
+
+}
