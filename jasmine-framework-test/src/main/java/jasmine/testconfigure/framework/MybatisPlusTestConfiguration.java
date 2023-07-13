a83b820ad1b5d75d9195a03d1d14c377a203ff49
magicodex <zhuangmaohui@163.com>
2023-07-13 22:32:22 +0800

    完善代码

--- jasmine-framework-test/src/main/java/jasmine/testconfigure/framework/MybatisPlusTestConfiguration.java
+++ jasmine-framework-test/src/main/java/jasmine/testconfigure/framework/MybatisPlusTestConfiguration.java
@@ -1,29 +1,33 @@
 package jasmine.testconfigure.framework;
 
+import cn.hutool.core.util.ArrayUtil;
 import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.core.toolkit.GlobalConfigUtils;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import jasmine.framework.database.mybatisplus.crypto.CryptoProvider;
-import jasmine.mock.framework.database.MockCryptoProvider;
 import jasmine.framework.database.mybatisplus.BaseEntityMetaObjectHandler;
 import jasmine.framework.database.mybatisplus.MybatisPlusInterceptorBuilder;
 import jasmine.framework.database.mybatisplus.crypto.CryptoFieldUtil;
+import jasmine.framework.database.mybatisplus.crypto.CryptoProvider;
 import jasmine.framework.database.mybatisplus.i18n.DefaultI18nEntityFacade;
 import jasmine.framework.database.mybatisplus.i18n.I18nEntityFacade;
 import jasmine.framework.database.mybatisplus.i18n.I18nEntityUtil;
 import jasmine.framework.database.mybatisplus.tenant.DefaultTenantLineHandler;
 import jasmine.framework.database.mybatisplus.tenant.IgnoreTableStrategy;
 import jasmine.framework.database.mybatisplus.tenant.TenantConfigProcessorScanBean;
+import jasmine.mock.framework.database.MockCryptoProvider;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 
 /**
  * @author mh.z
@@ -33,17 +37,26 @@ public class MybatisPlusTestConfiguration {
     @Value("${jasmine.data.tenant.enabled:false}")
     private Boolean tenantEnabled;
 
+    @Value("${mybatis-plus.mapperLocations:classpath*:/jasmine/**/mapper/*.xml}")
+    private String[] mapperLocations;
+
     @Bean
-    public MybatisSqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+    public MybatisSqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws IOException {
         MybatisSqlSessionFactoryBean factoryBean = new MybatisSqlSessionFactoryBean();
+
+        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        Resource[][] resourceArray = new Resource[mapperLocations.length][];
+        for (int index = 0; index < mapperLocations.length; index++) {
+            resourceArray[index] = resourceResolver.getResources(mapperLocations[index]);
+        }
 
         // 数据源
         factoryBean.setDataSource(dataSource);
         // 拦截器
         factoryBean.setPlugins(mybatisPlusInterceptor());
         // mapper文件路径
-        ClassPathResource mapperLocation = new ClassPathResource("jasmine/framework/mapper/DataAuthMapper.xml");
-        factoryBean.setMapperLocations(mapperLocation);
+        Resource[] mapperResources = ArrayUtil.addAll(resourceArray);
+        factoryBean.setMapperLocations(mapperResources);
 
         GlobalConfig globalConfig = GlobalConfigUtils.defaults();
         globalConfig.setMetaObjectHandler(metaObjectHandler());
