09b6dcba5001e86e41a2ec4726b6a0c1b7ce40b8
magicodex <zhuangmaohui@163.com>
2023-07-23 01:32:07 +0800

    完善代码

--- jasmine-framework/src/main/java/jasmine/framework/database/impl/datasource/MultipleDataSource.java
+++ jasmine-framework/src/main/java/jasmine/framework/database/impl/datasource/MultipleDataSource.java
@@ -4,12 +4,17 @@ import jasmine.framework.database.datasource.DataSourceContext;
 import jasmine.framework.database.datasource.DataSourceContextHolder;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+import java.util.Collections;
 import java.util.Map;
 
 /**
  * @author mh.z
  */
 public class MultipleDataSource extends AbstractRoutingDataSource {
+
+    public MultipleDataSource() {
+        setTargetDataSources(Collections.emptyMap());
+    }
 
     public MultipleDataSource(Map<Object, Object> dataSources) {
         setTargetDataSources(dataSources);
