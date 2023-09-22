c7dd00cb5487f589ff33e629bf099d3aa915f3c0
magicodex <zhuangmaohui@163.com>
2023-09-22 11:16:24 +0800

    完善代码

--- jasmine-security/src/main/java/jasmine/security/rbac/model/SecMenu.java
+++ jasmine-security/src/main/java/jasmine/security/rbac/model/SecMenu.java
@@ -33,12 +33,24 @@ public class SecMenu extends BaseI18nEntity {
     @TableField("menu_name")
     private String menuName;
 
+    /** 菜单图标 */
+    @TableField(value = "menu_icon", updateStrategy = FieldStrategy.IGNORED)
+    private String menuIcon;
+
+    /** 菜单路径 */
+    @TableField(value = "menu_path", updateStrategy = FieldStrategy.IGNORED)
+    private String menuPath;
+
     /** 菜单序号 */
     @TableField("menu_order")
     private Integer menuOrder;
 
+    /** 链接功能ID */
+    @TableField(value = "link_function_id", updateStrategy = FieldStrategy.IGNORED)
+    private Long linkFunctionId;
+
     /** 链接资源ID */
-    @TableField("link_resource_id")
+    @TableField(value = "link_resource_id", updateStrategy = FieldStrategy.IGNORED)
     private Long linkResourceId;
 
     /** 租户ID */
@@ -77,12 +89,36 @@ public class SecMenu extends BaseI18nEntity {
         this.menuName = menuName;
     }
 
+    public String getMenuIcon() {
+        return menuIcon;
+    }
+
+    public void setMenuIcon(String menuIcon) {
+        this.menuIcon = menuIcon;
+    }
+
+    public String getMenuPath() {
+        return menuPath;
+    }
+
+    public void setMenuPath(String menuPath) {
+        this.menuPath = menuPath;
+    }
+
     public Integer getMenuOrder() {
         return menuOrder;
     }
 
     public void setMenuOrder(Integer menuOrder) {
         this.menuOrder = menuOrder;
+    }
+
+    public Long getLinkFunctionId() {
+        return linkFunctionId;
+    }
+
+    public void setLinkFunctionId(Long linkFunctionId) {
+        this.linkFunctionId = linkFunctionId;
     }
 
     public Long getLinkResourceId() {
